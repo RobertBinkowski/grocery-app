a26182dfc0916c878180086e8ede178bea81e895
Robert Binkowski <56690174+RobertBinkowski@users.noreply.github.com>
2021-04-15 23:39:56 +0100

    Nav Bar

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # grocery-app
+
 Grocery App
+
+Grocery shopping receipt scanner that can convert receipts into text. Allows users to classify items (it should look up repeatedly bought items so user does not have to classify them each time they are bought) into categories. Users should be able to specify categories that they want to keep track of, and a number of different options would be possible (e.g., running total, average weekly/monthly spend per category, frequently purchased items, etc.), along with other features that could be useful. App should support different shops, and recognise the shop from the name and address on the receipt.
---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE HTML>
+<html lang="en-UK">
+<head>
+    <!-- Font --------------------------------------------------->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Comfortaa&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Baloo+Da+2&display=swap" rel="stylesheet">
+    <!-- Imports ------------------------------------------------>
+    <link href="CSS/root.css" rel="stylesheet" type="text/css" media="all" /><!-- Root-->
+    <!-- Rest --------------------------------------------------->
+    <meta name="Robert Binkowski" content="Personal Web Site">
+    <link rel="icon" href="IMG/logo.png">
+    <title>Robert Binkowski</title>
+</head>
+<body>
+    <!---------------------------------------------------- Nav -->
+    <?php include 'PHP/nav.html'; ?>
+    <!--------------------------------------------------- Main -->
+    <main>
+        <section class="main">
+            <h2>Robert Binkowski</h2>
+        </section>
+        <section>
+            <img src="" class="language" height="100" width="100" style="background: blue;">
+        </section>
+    </main>
+    <!------------------------------------------------- Footer -->
+    <?php include 'PHP/footer.html' ?>
+</body>
+</html>
